aa70dadcba64e39c8ef89d090cfc13753d2199e6
0happiness0 <99105884+0happiness0@users.noreply.github.com>
2022-06-22 01:16:36 +0900

    1

--- hansot/js.js
+++ hansot/js.js
@@ -89,77 +89,69 @@ function loadFn() {
     if (idx !== 0) ele.classList.add("scAct");
   });
 
-window.addEventListener('scroll', () => {
+  window.addEventListener('scroll', () => {
 
-  // 스크롤 위치표시
-  scTop = this.scrollY;
+    // 스크롤 위치표시
+    scTop = this.scrollY;
 
-  if (scTop >= 100)
-    slimTop.classList.add("on");
-  // 100px미만일 경우 클래스 "on" 제거
-  else
-    slimTop.classList.remove("on");
-  if (scTop >= 300)
-    tbtn.classList.add("on");
-  // 300px미만일 경우 클래스 "on" 제거
-  else
-    tbtn.classList.remove("on");
-});
+    if (scTop >= 100)
+      slimTop.classList.add("on");
+    // 100px미만일 경우 클래스 "on" 제거
+    else
+      slimTop.classList.remove("on");
+    if (scTop >= 300)
+      tbtn.classList.add("on");
+    // 300px미만일 경우 클래스 "on" 제거
+    else
+      tbtn.classList.remove("on");
+  });
 
 }
 
 // 팝업창 닫기
 function popupClose() { //팝업창 지우기
   $('.ad_banner').hide();
-  PopupBgDisplay(); 
+  PopupBgDisplay();
 }
 
 // 하루동안 열지 않기
-// 화면 시작 시 쿠키 값이 있는지 없는지를 확인
-function setCookie(name, value, expiredays){
-	var today = new Date();
+$(document).ready(function () {
+  $(".notToday").click(function () {
+      setCookieMobile( "todayCookie", "done" , 1);
+      $(".ad_banner").hide();
+  });
+});
 
-	console.log(today.getDate())
-
-	today.setDate(today.getDate() + expiredays); // 현재시간에 하루를 더함 
-
-	document.cookie = name + '=' + escape(value) + '; expires=' + today.toGMTString();
-
+function setCookieMobile ( name, value, expiredays ) {
+  var todayDate = new Date();
+  todayDate.setDate( todayDate.getDate() + expiredays );
+  document.cookie = name + "=" + escape( value ) + "; path=/; expires=" + todayDate.toGMTString() + ";"
 }
-	
-function getCookie(name) {
-
-	var cookie = document.cookie;
-	
-	if (document.cookie != "") {
-		var cookie_array = cookie.split("; ");
-		console.log(cookie_array)
-		for ( var index in cookie_array) {
-			var cookie_name = cookie_array[index].split("=");
-			if (cookie_name[0] == "mycookie") {
-				return cookie_name[1];
-			}
-		}
-	}
-	return;
+function getCookieMobile () {
+  var cookiedata = document.cookie;
+  if ( cookiedata.indexOf("todayCookier=done") < 0 ){
+       $(".ad_banner").show();
+  }
+  else {
+      $(".ad_banner").hide();
+  }
 }
-// 쿠키 값이 없으면 모달창 보이고, 있으면 숨김
-$(".notToday").click(function() {
-	$(".ad_banner").modal("hide");
-	setCookie("mycookie", 'popupEnd', 1);
-})
-
-var checkCookie = getCookie("mycookie");
-	
-if(checkCookie == 'popupEnd') {
-	$(".ad_banner").modal("hide");
-} else {
-	$('.ad_banner').modal("show");	
-}
+getCookieMobile();
 
 // 햄버거 버튼
-function showGnb(){
+function showGnb() {
   let tg = document.querySelector(".top");
   tg.classList.toggle("on");
 
 }
+
+// 하단영역 사업자 정보
+function ceo_info() {
+  if ($('.m_addr').css('display') == 'block') {
+    $('.m_addr').css('display', 'none');
+  } else {
+    $('.m_addr').css('display', 'block');
+  }
+}
+
+
